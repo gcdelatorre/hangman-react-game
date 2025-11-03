105862fa0011cdbd411d0ed3cb5b5b1346db972a
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-11-03 21:16:15 +0800

    `Added clsx dependency and implemented hangman game logic`

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import WinStatus from "./components/WinStatus"
 import Header from "./components/Header"
 import { nanoid } from "nanoid"
 import LanguageElements from "./components/LanguagesElements"
+import clsx from "clsx"
 
 export default function AssemblyEndgame() {
+
+    const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     const languageElements = languages.map(language => {
       const styles = {
@@ -20,6 +26,35 @@ export default function AssemblyEndgame() {
         />
     })
 
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
+                [...prevLetters, letter]
+        )
+    }
+
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>{guessedLetters.includes(letter) ? letter.toUpperCase() : ""}</span>
+    ))
+
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+                
+        return (<button
+            className={clsx(
+                isCorrect && "correct",
+                isWrong && "wrong"
+            )}
+            key={letter}
+            onClick={() => addGuessedLetter(letter)}
+        >
+            {letter.toUpperCase()}
+        </button>)
+    })
+
     return (
         <main>
 
@@ -31,6 +66,13 @@ export default function AssemblyEndgame() {
               <div className="languages-container">
                   {languageElements}
                 </div>
+            </section>
+
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
             </section>
             
         </main>
